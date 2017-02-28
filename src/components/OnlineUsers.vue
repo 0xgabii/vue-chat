486adb83cd60e93935d247467a5656ac09930319
Go Eun-Seok <gomonk3037@gmail.com>
2017-02-28 09:05:54 +0900

    add ViewOriginal Component

---
+++ src/components/OnlineUsers.vue
@@ -0,0 +1,17 @@
+<template> 
+  <div class="onlineUsers">
+  
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'onlineusers'
+  }
+</script>
+
+<style scoped>
+.onlineUsers {
+
+}
+</style>

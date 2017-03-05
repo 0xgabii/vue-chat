75a4cd5768d69a4e9b230e016a3d394b6b21b347
gomonk3037 <gomonk3037@gmail.com>
2017-03-06 00:50:43 +0900

    refactoring App Component

--- src/App.vue
+++ src/App.vue
@@ -11,33 +11,13 @@
 
 <script>
 import { mapActions, mapState } from 'vuex'
-import Firebase from './firebaseHelper'
 import LandingPage from './components/LandingPage'
 import ChatPage from './components/ChatPage'
-
-// set Firebase
-const database = Firebase.database();
-const provider = new Firebase.auth.GoogleAuthProvider();
 
 export default {
   name: 'app',
   created() {
-    Firebase.auth().onAuthStateChanged(user => {
-      if(user) {
-        const userObj = {
-          uid: user.uid,
-          username: user.displayName,
-          picture : user.photoURL,
-        }        
-        database.ref('users/' + user.uid).set(userObj);
-        database.ref('online/' + user.uid).set(userObj);
-
-        this.setMyAccount(userObj); 
-        this.setAuth(true);
-      } else{
-        this.setAuth(false);
-      }
-    }); 
+    this.checkAuth();
   },
   computed: {
     ...mapState([
@@ -52,17 +32,10 @@ export default {
   },
   methods: {
     ...mapActions([
-      'setMyAccount',
-      'setAuth',
+      'checkAuth',
+      'login',
+      'logout',
     ]),
-    login() {
-      Firebase.auth().signInWithRedirect(provider);        
-    },
-    logout() {
-      this.setAuth(false);
-      database.ref('online/' + this.myAccount.uid).remove();
-      Firebase.auth().signOut();
-    },
   },
   components: {
     ChatPage,
--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,3 +1,9 @@
+import Firebase from '../../firebaseHelper';
+
+// set Firebase
+const database = Firebase.database();
+const provider = new Firebase.auth.GoogleAuthProvider();
+
 export default {
   state: {
     auth: true,
@@ -10,13 +16,41 @@ export default {
     SET_MYACCOUNT (state, obj) {
       state.myAccount = obj;
     },
+    LOGIN(state) {
+      Firebase.auth().signInWithRedirect(provider);
+    },
+    LOGOUT(state) {
+      state.auth = false;
+      database.ref('online/' + state.myAccount.uid).remove();
+      Firebase.auth().signOut();
+      state.myAccount = {};
+    }
   },
   actions: {
-   setMyAccount ({commit}, obj) {
-     commit('SET_MYACCOUNT', obj);
+   checkAuth({ commit }) {
+    Firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        const userObj = {
+          uid: user.uid,
+          username: user.displayName,
+          picture : user.photoURL,
+        };
+
+        database.ref('users/' + user.uid).set(userObj);
+        database.ref('online/' + user.uid).set(userObj);
+
+        commit('SET_MYACCOUNT', userObj);
+        commit('SET_AUTH', true);
+      } else{
+        commit('SET_AUTH', false);
+      }
+    }); 
    },
-   setAuth ({commit}, value) {
-     commit('SET_AUTH', value);
-   }
+   login({ commit }) {
+     commit('LOGIN');
+   },
+   logout({ commit }) {
+     commit('LOGOUT');     
+   },
   }
 }

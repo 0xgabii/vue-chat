fe3d6dc9de892f323fc702fcde57f705212c1c65
gomonk3037 <gomonk3037@gmail.com>
2017-03-08 05:51:32 +0900

    add style to progress

--- src/components/ChatPage/uploadProgress.vue
+++ src/components/ChatPage/uploadProgress.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="upload-progress">
-
+    <div class="progress" :style="{width: '75%'}" />
+    <span class="progress-text">100%</span>
   </div>
 </template>
 
@@ -11,5 +12,32 @@ export default {
 </script>
 
 <style>
-
+.upload-progress {
+  position: absolute;
+  right: 2rem;
+  bottom: 5.5rem;
+  width: 15rem;
+  height: 1.5rem;
+  background-color: rgba(0, 0, 0, 0.8);
+  border-radius: 1rem;  
+  box-shadow: 0 3px 3px rgba(0,0,0,0.2), 0 3px 3px rgba(0,0,0,0.2);
+}
+.progress {  
+  position: absolute;
+  left: 0.5rem;
+  top: 50%;
+  height: 0.65rem;
+  background-color: #34c43a;
+  border-radius: 1rem;
+  transform: translateY(-50%);  
+}
+.progress-text {
+  position: absolute;
+  right: 0.5rem;
+  top: 50%;
+  color: white;
+  font-weight: 300;
+  font-size: 0.9rem;  
+  transform: translateY(-50%);
+}
 </style>

a967c05a1436f9bac3e2cf7c470f6eaa32333a53
gomonk3037 <gomonk3037@gmail.com>
2017-03-08 06:33:57 +0900

    complete uploadprogress store module

--- src/store/modules/uploadprogress.js
+++ src/store/modules/uploadprogress.js
@@ -1,11 +1,24 @@
 export default {
+  namespaced: true,
   state: {
-
+    progress: 0
   },
   mutations: {
-
+    UPDATE_PROGRESS(state, value) {
+      state.progress = value;
+    },
+    CLEAR_PROGRESS(state) {
+      state.progress = 0;
+    }
   },
   actions: {
-    
+    updateProgress({ commit }, value) {
+      commit('UPDATE_PROGRESS', value);
+      if(value === 100) {
+        setTimeout(() => {
+          commit('CLEAR_PROGRESS');
+        }, 1000);
+      }
+    }
   }
 }

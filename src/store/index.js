8deb0fd87c86d267c74857468ec2c87e3ea575f7
gomonk3037 <gomonk3037@gmail.com>
2017-03-08 02:42:57 +0900

    add promise polyfill for IE11

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import 'es6-promise/auto'; // promise polyfill for IE11
+
 import app from './modules/app';
 import colorpicker from './modules/colorpicker';
 import modals from './modules/modals';
